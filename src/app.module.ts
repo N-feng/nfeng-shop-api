271068c63478dbd4eb7cf859fdcf1ce93c5e107f
N-feng <nf308561157@gmail.com>
2025-03-12 17:54:47 +0800

    feat✨: delete common&db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CommonModule } from './common/common.module';
-import { ProjectController } from './project/project.controller';
-import { ProjectService } from './project/project.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ProjectModule } from './project/project.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    // CommonModule,
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: async () => ({
-    //     type: 'mongodb',
-    //     // host: process.env.DB_HOST,
-    //     // port: 27017,
-    //     // username: process.env.DB_USERNAME,
-    //     // password: process.env.DB_PASSWORD,
-    //     // database: process.env.DB_DATABASE, // 数据库名
-    //     // url: process.env.MONGO_URI,
-    //     entities: [join(__dirname, '**/entity/*.{ts,js}')], // 需要自动实体映射的文件路径匹配
-    //     useNewUrlParser: true, // 使用新版mongo连接Url解析格式
-    //     synchronize: true, // 自动同步数据库生成entity
-    //   })
-    // }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entity/project.entity';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
 import { ProjectResolver } from './project.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ProjectSchema } from 'src/db/schemas/project.schema';
+import { ProjectSchema } from './entity/project.schema';
 
 @Module({
   imports: [
-    // TypeOrmModule.forFeature([Project]),
     MongooseModule.forFeature([
       {
         name: Project.name,
